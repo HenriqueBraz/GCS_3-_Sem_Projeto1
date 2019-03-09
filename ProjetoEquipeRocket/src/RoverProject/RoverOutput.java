8decc53ac8c325331d57e00a82947a91a018a609
Unknown <henrique.braz@acad.pucrs.br>
2019-03-09 15:53:17 -0300

    Renomeando Classes

--- ProjetoEquipeRocket/src/RoverProject/RoverOutput.java
+++ ProjetoEquipeRocket/src/RoverProject/RoverOutput.java
@@ -5,28 +5,10 @@
  */
 package RoverProject;
 
-import java.util.Arrays;
-
 /**
  *
  * @author Henrique
  */
-public class RoverOutput {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-         String[] comandos = {"L","M","L","M","L","M","L","M","M"}; 
-         BrainRover.grid( 5, 5 );
-         BrainRover.initialPosition(1, 2,"N");
-         System.out.println(Arrays.toString(BrainRover.roverMove(comandos)));//1 3 N
-         BrainRover.initialPosition(3, 3,"E");
-         String[] comandos2 = {"M","M","R","M","M","R","M","R","R","M"};
-         System.out.println(Arrays.toString(BrainRover.roverMove(comandos2))); //5 1 E         
-        
-    }
+public class RoverInput {
     
 }
