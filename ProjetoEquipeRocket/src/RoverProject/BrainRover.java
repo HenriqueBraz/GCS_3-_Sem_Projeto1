d7870d357cd9e8fa46ed02071b7a80c1c3dd8db5
Unknown <henrique.braz@acad.pucrs.br>
2019-03-10 21:29:25 -0300

    implementado função platoTest(), que testaas bordas do plato e caso esteja fora das coordenadas, Rover não executará a sequencia de comandos, retornando uma menssagem de aviso e código [-2]

--- ProjetoEquipeRocket/src/RoverProject/BrainRover.java
+++ ProjetoEquipeRocket/src/RoverProject/BrainRover.java
@@ -9,8 +9,8 @@ package RoverProject;
  *Legenda de retornos:
  * 1 ---> OK
  * -1 ---> RuntimeException
- * -2 ---> Erro de coordenadas fora do platô, colisão ou
- * queda (o Rover não executará a ação para erros de código2)
+ * -2 ---> Erro de coordenadas fora do platô  ou perigo de
+ * queda (o Rover não executará a ação para erros de código -2)
  * Os pontos cardeais N,S,E,W devem ser uma String upperCase
  * @author Henrique
  */
@@ -20,7 +20,32 @@ public class BrainRover {
     private static int lengthy = 0;
     private static int x = 0;
     private static int y = 0;
+    private static int xBackup = 0;
+    private static int yBackup = 0;
+     private static String positionBackup = "";
     private static String position = "";
+    
+    /**
+     *Função para testar os limites do platô
+     * a e b devem ser inteiros
+     * Se retornar -2, Rover não executa a cadeia de comandos,
+     * permanecendo na posição inicial
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int platoTest(int a, int b){
+        
+       if (x > lengthx || x < 0 || y > lengthy || y < 0) {
+                x = xBackup;
+                y = yBackup;
+                position = positionBackup;
+                System.out.println("Warning: Cadeia de comandos não executada - perigo de queda");
+                System.out.println("posição atual: ["+xBackup+", "+yBackup+", "+positionBackup+"]");
+                return -2;
+       }
+       return 1;
+    }
 
     /**
      *parâmetros a e b devem ser inteiros, esse é o tamanho da grade.
@@ -33,11 +58,10 @@ public class BrainRover {
     public static int grid(int a, int b) {
 
         try {
-
             lengthx = a;
             lengthy = b;
             return 1;
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
 
             System.out.println("Erro: -1: " + e.getMessage());
             return -1;
@@ -47,7 +71,7 @@ public class BrainRover {
     /**
      * Posição inicial do Rover, a representa posição no eixo x
      * e b posição no eixo y
-     * s representa o ponto cardeal no qual o Rver está apontado e deve ser 
+     * s representa o ponto cardeal no qual o Rover está apontado e deve ser 
      * uma String upperCase (N,S,E,W)
      * @param a
      * @param b
@@ -61,12 +85,14 @@ public class BrainRover {
                 System.out.println("Coordenadas fora do platô");
                 return -2;
             } else {
-
+                xBackup = a;
+                yBackup = b;
+                positionBackup = s;
                 x = a;
                 y = b;
                 position = s;
             }
-        } catch (NullPointerException e) {
+        } catch (Exception e) {
 
             System.out.println("Erro: -1: " + e.getMessage());
             return -1;
@@ -77,66 +103,77 @@ public class BrainRover {
     public static String[] roverMove(String[] comandos) {
 
         try {
-            for (int i = 0; i < comandos.length; i++) {
-                switch (comandos[i]) {
-
+            for (String comando : comandos) {
+                switch (comando) {
                     case "L":
-                        if (position == "N") {
-                            position = "W";
-                        }
-                        else if (position == "S") {
-                            position = "E";
-                        }
-                        else if (position == "E") {
-                            position = "N"; 
-                        }
-                        else if (position == "W") {
-                            position = "S";
-                        }
+                        if (null != position) switch (position) {
+                    case "N":
+                        position = "W";
                         break;
-                        
-                    case "R":
-                        if (position == "N") {
-                            position = "E";
-                        }
-                        else if (position == "S") {
-                            position = "W";
-                        }
-                        else if (position == "E") {
-                            position = "S"; 
-                        }
-                        else if (position == "W") {
-                            position = "N";
-                        }
+                    case "S":
+                        position = "E";
                         break;
-                        
-                    case "M":
-                        if (position == "N") {
-                            y++;
-                        }
-                        else if (position == "S") {
-                            y--;
-                        }
-                        else if (position == "E") {
-                            x++; 
-                        }
-                        else if (position == "W") {
-                            x--; 
-                        }
+                    case "E":
+                        position = "N";
                         break;
-                        
+                    case "W":
+                        position = "S";
+                        break;
+                    default:
+                        break;
                 }
-
+                        break;
+                    case "R":
+                        if (null != position) switch (position) {
+                    case "N":
+                        position = "E";
+                        break;
+                    case "S":
+                        position = "W";
+                        break;
+                    case "E":
+                        position = "S";
+                        break;
+                    case "W":
+                        position = "N";
+                        break;
+                    default:
+                        break;
+                }
+                        break;
+                    case "M":
+                        if (null != position) switch (position) {
+                    case "N":
+                        y++;
+                        break;
+                    case "S":
+                        y--;
+                        break;
+                    case "E":
+                        x++;
+                        break;
+                    case "W":
+                        x--;
+                        break;
+                    default:
+                        break;
+                }
+                        break;
+                }
             }
-        String stringX = Integer.toString(x);
-        String stringY = Integer.toString(y);
-        String [] retorno = {stringX,stringY,position};    
-        return retorno;
-        }catch(NullPointerException e){
+            if(platoTest(x,y) == -2){
+            }else{
+            String stringX = Integer.toString(x);
+            String stringY = Integer.toString(y);
+            String [] retorno = {stringX,stringY,position};    
+            return retorno;
+            }
+        }catch(Exception e){
                 
             System.out.println("Erro -1:" + e.getMessage());
         }
-        return null;
+        String[] retorno = {"-2"};
+        return retorno;
     }
 
 }
